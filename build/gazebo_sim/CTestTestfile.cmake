67957e521d481c10c007c718ae2d5def9921f331
Robopunk <87967355+DOMRABBIT@users.noreply.github.com>
2023-09-25 17:49:28 +0800

    Delete build directory

--- build/gazebo_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/crp/git/quad_sys/src/gazebo_sim
-# Build directory: /home/crp/git/quad_sys/build/gazebo_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/unitree_legged_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/crp/git/quad_sys/src/unitree_legged_msgs
-# Build directory: /home/crp/git/quad_sys/build/unitree_legged_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
