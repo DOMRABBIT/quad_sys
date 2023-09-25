94ce101fc4f9fd3dd0613593741a9cd3fcc883b5
Robopunk <87967355+DOMRABBIT@users.noreply.github.com>
2023-09-25 17:49:36 +0800

    Delete devel directory

--- devel/share/unitree_legged_msgs/cmake/unitree_legged_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(unitree_legged_msgs_MSG_INCLUDE_DIRS "/home/crp/git/quad_sys/src/unitree_legged_msgs/msg")
-set(unitree_legged_msgs_MSG_DEPENDENCIES std_msgs;geometry_msgs;sensor_msgs)
